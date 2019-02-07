83db4b1bea21068ea22a7f20bd7b2d5f132f6af8
Neetha Jambigi <neetha.jambigi@st.ovgu.de>
2019-02-07 17:07:51 +0100

    updates

--- snippets/mnb_entity.py
+++ snippets/mnb_entity.py
@@ -1,85 +1,42 @@
 import pandas as pd
-from datetime import datetime
-
-import sys
-import os
-
-sys.path.append('../')
-
-import snippets.amazon_constants_file_paths as paths
 
 
+class Entity:
 
+    df = None
+    class_counts = {'negative': 0, 'positive': 0, 'neutral': 0}
 
+    # create a new data frame when encountered a new product ID
+    def __init__(self, preprocessed_word_dict, class_list):
 
+        # create empty data frame with class labels as index column
+        self.df = pd.DataFrame({'Class': class_list}).set_index('Class')
+        # read all words to make column list
+        column_list = [key for key in preprocessed_word_dict.keys()]
+        # create columns with the words and set them to 0
+        for column in column_list:
+            self.df[column] = 0
 
-if __name__ == '__main__':
+    def learn(self, preprocessed_word_dict, review_class):
 
-    folder = paths.DIR_AMAZON
-    print("Read the clothing, shoes and jewelry dataset")
-    clothing = pd.read_pickle(os.path.join(folder, "reviews_Clothing_Shoes_and_Jewelry_5.pkl.gzip"))
+        for term, count in preprocessed_word_dict.items():
+            # add a term that is not existing
+            if term not in self.df:
+                self.df[term] = 0
+            # update counts for word in a class
+            self.df.loc[review_class][term] += count
 
-    # ------------------------ construct data stream ----------------------------
-    #data = pd.concat([beauty, phones, clothing, electronics, home, apps, cds, health, kindle])
-    data = pd.concat([clothing])
+        # increment class count
+        self.class_counts[review_class] += 1
 
-    data.reset_index(inplace=True, drop=False)
-    data.set_index("date", inplace=True)
-    data.sort_index(inplace=True)
+    def unlearn_review(self, preprocessed_word_dict, review_class):
 
-    # considering data only from 2011
-    data = data.loc["2011":, :]
+        for term, count in preprocessed_word_dict.items():
+            # update counts for word in a class
+            self.df.loc[review_class][term] -= count
 
-    # setting the categories to be chosen for each quarter in the year
-    print(data.head())
+        # decrement class count
+        self.class_counts[review_class] -= 1
 
-
-    # setting the categories to be chosen for each quarter in the year
-    product_categories = {
-        datetime(2011, 1, 31): ['clothing, shoes and jewelry'],
-        datetime(2011, 4, 30): ['clothing, shoes and jewelry'],
-        datetime(2011, 7, 31): ['clothing, shoes and jewelry'],
-        datetime(2011, 10, 31): ['clothing, shoes and jewelry'],
-        datetime(2012, 1, 31): ['clothing, shoes and jewelry'],
-        datetime(2012, 4, 30): ['clothing, shoes and jewelry'],
-        datetime(2012, 7, 31): ['clothing, shoes and jewelry'],
-        datetime(2012, 10, 31): ['clothing, shoes and jewelry'],
-        datetime(2013, 1, 31): ['clothing, shoes and jewelry'],
-        datetime(2013, 4, 30): ['clothing, shoes and jewelry'],
-        datetime(2013, 7, 31): ['clothing, shoes and jewelry'],
-        datetime(2013, 10, 31): ['clothing, shoes and jewelry'],
-        datetime(2014, 1, 31): ['clothing, shoes and jewelry'],
-        datetime(2014, 4, 30): ['clothing, shoes and jewelry'],
-        datetime(2014, 7, 31): ['clothing, shoes and jewelry']
-    }
-
-    # create three month groups
-    grouped = data.groupby(pd.Grouper(freq='3M'))
-
-    filtered_data = []
-    for name, group in grouped:
-        # get the categories
-        categories = group.loc[:, "category"].unique()
-
-        # randomly select 3 categories
-        selected = product_categories[name]
-
-        # filter for selected categories
-        selected_data = group[group['category'].isin(selected)]
-
-        # add to result list
-        filtered_data.append(selected_data)
-
-    filtered_data = pd.concat(filtered_data)
-
-    # remove duplicates
-    filtered_data = filtered_data.reset_index(drop=False).drop_duplicates(["date", "review_id"]).set_index(
-        ["date", "review_id"])
-
-    # save
-    filtered_data.reset_index(drop=False, inplace=True)
-    filtered_data.set_index(["date", "review_id"], inplace=True)
-    filtered_data.sort_index(inplace=True)
-    filtered_data.to_pickle(os.path.join(folder, "filtered", "reviews.pkl.gzip"))
-
-
+    def print(self):
+        print(self.df)
--- snippets/mnb_entity_1.py
+++ snippets/mnb_entity_1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import mnb_entity
 
 # TO DO:
 # def Get_class_counts
@@ -12,124 +13,143 @@ import pandas as pd
 class MultinomialNB:
 
     class_list = ['negative', 'positive', 'neutral']
-    global_entity_dictionary = {}
-    global_term_frequency = pd.DataFrame
     class_counts = {'negative': 0, 'positive': 0, 'neutral': 0}
-    learning_mode_flag = 0
 
-    # create 1 global dataframe which will contain all terms frequencies, all classes  in 1 dataframe for all the
+    # this dictionary will contain all the entities
+    global_entity_dictionary = {}
+
+    # global dataframe which will contain all terms frequencies, all classes in 1 dataframe for all the
     # entities, use this dataframe to predict for a new entity that has no history
-    def create_global_dataframe(self):
-        return pd.DataFrame({'Class': self.class_list}).set_index('Class')
+    global_term_frequency = pd.DataFrame({'Class': class_list}).set_index('Class')
 
-    # def __init__(self):
-    # read the class Series, get all unique values, initialize in class list
+    # update counts in data frame when encountering an existing product ID
+    def learn(self, entity_id, preprocessed_word_dict, review_class):
 
-    # create a new data frame when encountered a new product ID
-    def create_new_df(self, entity_id, preprocessed_word_dict):
-        # create empty data frame with class labels as index column
-        df = pd.DataFrame({'Class': self.class_list}).set_index('Class')
+        # if new entity, create a new Entity object and add the new object to global_entity_dictionary
+        if entity_id not in self.global_entity_dictionary.keys():
+            self.global_entity_dictionary[entity_id] = mnb_entity.Entity(preprocessed_word_dict, self.class_list)
 
-        # read all words to make column list
-        column_list = [key for key in preprocessed_word_dict.keys()]
-        # create columns with the words and set them to 0
-        for column in column_list:
-            df[column] = 0
+        # update global class counts
+        self.class_counts[review_class] += 1
 
-        # assign the new data frame to the global dictionary
-        self.global_entity_dictionary[entity_id] = df
+        # update global term frequency
+        for term, count in preprocessed_word_dict.items():
+            # add a term that is not existing
+            if term not in self.global_term_frequency:
+                self.global_term_frequency[term] = 0
+            # update counts for word in a class
+            self.global_term_frequency.loc[review_class][term] += count
 
-    # update counts in data frame when encountered a existing product ID
-    def update_existing_df(self, entity_id, preprocessed_word_dict, review_class, operation='learn'):
-        # for every word in the dictionary, add a column to dataframe, if term doesnt exist
-        if operation == 'learn':
-            for word, count in preprocessed_word_dict.items():
-                # add a term that is not existing
-                if word not in self.global_entity_dictionary[entity_id]:
-                    self.global_entity_dictionary[entity_id][word] = 0
-                # update counts for word in a class
-                self.global_entity_dictionary[entity_id].loc[review_class][word] += count
+        # add the example to the model
+        self.global_entity_dictionary[entity_id].learn(preprocessed_word_dict, review_class)
 
-            # increment class count
-            self.class_counts[review_class] += 1
+    def unlearn(self, entity_id, preprocessed_word_dict, review_class):
 
-        elif operation == 'unlearn':
-            for word, count in preprocessed_word_dict.items():
-                # update counts for word in a class
-                self.global_entity_dictionary[entity_id].loc[review_class][word] -= count
-                # decrement class count
-            self.class_counts[review_class] -= 1
+        # if entity does not exist yet, raise an exception
+        if entity_id not in self.global_entity_dictionary.keys():
+            raise ValueError("entity_id ", entity_id, " is not present in the model.")
 
+        # update global class counts
+        self.class_counts[review_class] -= 1
+
+        # update global term frequency
+        for term, count in preprocessed_word_dict.items():
+            # update counts for word in a class
+            self.global_term_frequency.loc[review_class][term] -= count
+
+        # remove the example from the model
+        self.global_entity_dictionary[entity_id].unlearn_review(preprocessed_word_dict, review_class)
 
 
 if __name__ == '__main__':
 
-    mnb = MultinomialNB()
-    # print(type(mnb.global_entity_dictionary))
-
-    # mnb.fit(X_train, y_train)
-
     entity_id1 = 'B00FRH4F66'
-    entity_id2 = ''
-    processed_word_dict = {
+    entity_id2 = 'B00AWMBZR8'
+    processed_word_dict1 = {
         'today': 1,
-         '-PRON-': 4,
-         'get': 1,
-         'like': 1,
-         'nice': 1,
-         'length': 1,
-         'think': 1,
-         'probably': 1,
-         'heavy': 1,
-         'but': 1,
-         'actually': 1,
-         'lite': 1,
-         'weight': 1,
-         'nice length': 1,
-         '-PRON- think': 1,
-         'actually lite': 1,
-         'lite weight': 1,
-         '-PRON- just get': 1,
-         'get -PRON- today': 1,
-         'today and -PRON-': 1,
-         '-PRON- really like': 1,
-         '-PRON- a nice': 1,
-         'probably be heavy': 1,
-         'but -PRON- actually': 1,
-         'actually lite weight': 1}
+        '-PRON-': 4,
+        'get': 1,
+        'like': 1,
+        'nice': 1,
+        'length': 1,
+        'think': 1,
+        'probably': 1,
+        'heavy': 1,
+        'but': 1,
+        'actually': 1,
+        'lite': 1,
+        'weight': 1,
+        'nice length': 1,
+        '-PRON- think': 1,
+        'actually lite': 1,
+        'lite weight': 1,
+        '-PRON- just get': 1,
+        'get -PRON- today': 1,
+        'today and -PRON-': 1,
+        '-PRON- really like': 1,
+        '-PRON- a nice': 1,
+        'probably be heavy': 1,
+        'but -PRON- actually': 1,
+        'actually lite weight': 1
+    }
 
-    processed_word_dict2 = {'daughter': 1,
- 'love': 1,
- 'retro': 1,
- 'fit': 1,
- 'bill': 1,
- 'perfectly': 1,
- 'daughter love': 1,
- 'love retro': 1,
- 'bill perfectly': 1,
- 'daughter love retro': 1,
- 'fit the bill': 1}
+    processed_word_dict2 = {
+        'daughter': 1,
+        'love': 1,
+        'retro': 1,
+        'fit': 1,
+        'bill': 1,
+        'perfectly': 1,
+        'daughter love': 1,
+        'love retro': 1,
+        'bill perfectly': 1,
+        'daughter love retro': 1,
+        'fit the bill': 1
+    }
 
-    mnb.create_new_df(entity_id1, processed_word_dict)
-    mnb.update_existing_df(entity_id1,processed_word_dict,'positive')
-    mnb.update_existing_df(entity_id1,processed_word_dict,'negative')
+    mnb = MultinomialNB()
 
+    mnb.learn(entity_id1, processed_word_dict1, 'positive')
+    mnb.learn(entity_id1, processed_word_dict1, 'negative')
 
-    mnb.create_new_df('B00AWMBZR8',processed_word_dict)
-    mnb.update_existing_df('B00AWMBZR8',processed_word_dict,'positive')
-    mnb.update_existing_df(entity_id1,processed_word_dict2,'negative')
-    mnb.update_existing_df('B00AWMBZR8',processed_word_dict2,'negative')
+    mnb.learn(entity_id2, processed_word_dict1, 'positive')
+    mnb.learn(entity_id2, processed_word_dict2, 'negative')
+
     print("BEFORE")
-    print(mnb.global_entity_dictionary['B00FRH4F66'])
-    print(mnb.class_counts)
+    mnb.global_entity_dictionary[entity_id1].print()
+    print(mnb.global_entity_dictionary[entity_id1].class_counts)
+    print('global term freq: \n', mnb.global_term_frequency)
     print("BEFORE")
-    mnb.update_existing_df(entity_id1,processed_word_dict2,'negative', 'unlearn')
 
+    mnb.unlearn(entity_id2, processed_word_dict2, 'negative')
+    mnb.unlearn(entity_id1, processed_word_dict1, 'negative')
 
     print("AFTER")
-    print(mnb.class_counts)
-    print(mnb.global_entity_dictionary['B00FRH4F66'])
+    mnb.global_entity_dictionary[entity_id1].print()
+    print(mnb.global_entity_dictionary[entity_id1].class_counts)
+    print('global term freq: \n', mnb.global_term_frequency)
     print("AFTER")
 
+'''
+# for every word in the dictionary, add a column to dataframe, if term doesnt exist
+if operation == 'learn':
+    for term, count in preprocessed_word_dict.items():
+        # add a term that is not existing
+        if term not in self.global_entity_dictionary[entity_id]:
+            self.global_entity_dictionary[entity_id][term] = 0
+        # update counts for word in a class
+        self.global_entity_dictionary[entity_id].loc[review_class][term] += count
 
+        # update the global term frequency dataframe
 
+    # increment class count
+    self.class_counts[review_class] += 1
+
+elif operation == 'unlearn':
+    for term, count in preprocessed_word_dict.items():
+        # update counts for word in a class
+        self.global_entity_dictionary[entity_id].loc[review_class][term] -= count
+
+    # decrement class count
+    self.class_counts[review_class] -= 1
+'''
